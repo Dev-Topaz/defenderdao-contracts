fe471e327338e33e6dd993e1c798f838b01f02a0
Efrain <efrainc.dev@gmail.com>
2022-08-08 10:03:36 -0500

    Created box and token  with  voting features smart contracts

---
+++ contracts/Box.sol
@@ -0,0 +1,24 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+// Import Ownable from the OpenZeppelin Contracts library
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+// Make Box inherit from the Ownable contract
+contract Box is Ownable {
+    uint256 private _value;
+
+    event ValueChanged(uint256 value);
+
+    // The onlyOwner modifier restricts who can call the store function
+    // Store new values in the contract
+    function store(uint256 value) public onlyOwner {
+        _value = value;
+        emit ValueChanged(value);
+    }
+    //Read the last stored 
+    function retrieve() public view returns (uint256) {
+        return _value;
+    }
+}
---
+++ contracts/DefenderToken.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact efrainc.dev@gmail.com
+contract DefenderToken is ERC20, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes {
+    constructor() ERC20("DefenderToken", "DFT") ERC20Permit("DefenderToken") {
+        _mint(msg.sender, 2000000 * 10 ** decimals());
+    }
+
+    function snapshot() public onlyOwner {
+        _snapshot();
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override(ERC20, ERC20Snapshot)
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _afterTokenTransfer(address from, address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address to, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
