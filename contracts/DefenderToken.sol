cf9b1ea3d7085c4b85052ea6dba49211dcf40464
Efrain <efrainc.dev@gmail.com>
2022-08-09 10:30:14 -0500

    Fixing ERC20 smart contract

--- contracts/DefenderToken.sol
+++ contracts/DefenderToken.sol
@@ -2,12 +2,30 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract DefenderToken is ERC20, ERC20Permit, ERC20Votes {
+contract DefenderToken is ERC20, Pausable, Ownable, ERC20Permit, ERC20Votes {
     constructor() ERC20("DefenderToken", "DFT") ERC20Permit("DefenderToken") {
         _mint(msg.sender, 2000000 * 10 ** decimals());
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
     }
 
     // The following functions are overrides required by Solidity.
