0ca4e25e2c855a46ca2cafc7e34cb9c7411fc510
Efrain <efrainc.dev@gmail.com>
2022-08-09 15:19:42 -0500

    smart contracts fix

--- contracts/DefenderToken.sol
+++ contracts/DefenderToken.sol
@@ -3,31 +3,25 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract DefenderToken is ERC20, ERC20Snapshot, AccessControl, Pausable, ERC20Permit, ERC20Votes {
-    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
+contract DefenderToken is ERC20, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes {
     constructor() ERC20("DefenderToken", "DFT") ERC20Permit("DefenderToken") {
-        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(SNAPSHOT_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
         _mint(msg.sender, 2000000 * 10 ** decimals());
     }
 
-    function snapshot() public onlyRole(SNAPSHOT_ROLE) {
+    function snapshot() public onlyOwner {
         _snapshot();
     }
 
-    function pause() public onlyRole(PAUSER_ROLE) {
+    function pause() public onlyOwner {
         _pause();
     }
 
-    function unpause() public onlyRole(PAUSER_ROLE) {
+    function unpause() public onlyOwner {
         _unpause();
     }
 
