d2cd6632fb6abd55ea5a6ba9a717cbb53ba7e73c
Efrain <efrainc.dev@gmail.com>
2022-08-09 10:18:05 -0500

    added defender governor , timelock and fixed ERC20 contract

--- contracts/DefenderToken.sol
+++ contracts/DefenderToken.sol
@@ -2,36 +2,12 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-/// @custom:security-contact efrainc.dev@gmail.com
-contract DefenderToken is ERC20, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes {
+contract DefenderToken is ERC20, ERC20Permit, ERC20Votes {
     constructor() ERC20("DefenderToken", "DFT") ERC20Permit("DefenderToken") {
         _mint(msg.sender, 2000000 * 10 ** decimals());
-    }
-
-    function snapshot() public onlyOwner {
-        _snapshot();
-    }
-
-    function pause() public onlyOwner {
-        _pause();
-    }
-
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount)
-        internal
-        whenNotPaused
-        override(ERC20, ERC20Snapshot)
-    {
-        super._beforeTokenTransfer(from, to, amount);
     }
 
     // The following functions are overrides required by Solidity.
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config();
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100,
+      },
+    },
+  },
   paths: { tests: "tests" },
 };
 
