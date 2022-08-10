c58324a7b41b80bed44da67d1304c2207f39e352
Efrain <efrainc.dev@gmail.com>
2022-08-09 22:12:39 -0500

    deployment script added

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,22 @@
-import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 dotenv.config();
 
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.9",
+    version: '0.8.9',
     settings: {
       optimizer: {
         enabled: true,
@@ -14,7 +24,21 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  paths: { tests: "tests" },
+  paths: { tests: 'tests' },
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_URL || '',
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
---
+++ scripts/DeployToken.ts
@@ -0,0 +1,50 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ethers } from 'ethers';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'dotenv/config';
+import * as DefenderTokenJson from '../artifacts/contracts/DefenderToken.sol/DefenderToken.json';
+
+if (process.env.PRIVATE_KEY === '' || process.env.MNEMONIC === '') {
+  console.warn('Must provide PRIVATE_KEY or MNEMONIC environment variable');
+  process.exit(1);
+}
+
+if (process.env.INFURA_PROJECT_ID === '') {
+  console.warn('Must provide INFURA_PROJECT_ID environment variable');
+  process.exit(1);
+}
+
+async function main() {
+  const wallet = process.env.MNEMONIC && process.env.MNEMONIC.length > 0
+    ? ethers.Wallet.fromMnemonic(process.env.MNEMONIC)
+    : new ethers.Wallet(process.env.PRIVATE_KEY!);
+  console.log(`Using address ${wallet.address}`);
+  // const provider = ethers.providers.getDefaultProvider("ropsten");
+  const provider = new ethers.providers.InfuraProvider(
+    'ropsten',
+    process.env.INFURA_PROJECT_ID,
+  );
+  const signer = wallet.connect(provider);
+  const balanceBN = await signer.getBalance();
+  const balance = Number(ethers.utils.formatEther(balanceBN));
+  console.log(`Wallet balance ${balance}`);
+  if (balance < 0.01) {
+    throw new Error('Not enough ether');
+  }
+  console.log('Deploying MyToken contract');
+  const tokenFactory = new ethers.ContractFactory(
+    DefenderTokenJson.abi,
+    DefenderTokenJson.bytecode,
+    signer,
+  );
+  const tokenContract = await tokenFactory.deploy();
+  console.log('Awaiting confirmations');
+  await tokenContract.deployed();
+  console.log('Completed');
+  console.log(`Contract deployed at ${tokenContract.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
